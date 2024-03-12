46935bcd09ff58dd8287987cfe3387e6e326c834
judemin <judemin2087@naver.com>
2024-03-12 15:07:51 +0900

    INIT : Init Spring Boot Project

---
+++ src/main/java/com/hearus/hearusspring/HearusSpringApplication.java
@@ -0,0 +1,13 @@
+package com.hearus.hearusspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearusSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HearusSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hearus/hearusspring/HearusSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hearus.hearusspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HearusSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
