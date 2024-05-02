84cf5780d33515d923901e43615440a446a6c168
judemin <judemin2087@naver.com>
2024-05-02 22:49:44 +0900

    UPDATE : Updated transcribe() socketio method

--- src/main/java/com/hearus/hearusspring/common/socket/NettySocketio.java
+++ src/main/java/com/hearus/hearusspring/common/socket/NettySocketio.java
@@ -9,7 +9,9 @@ import com.corundumstudio.socketio.listener.DisconnectListener;
 import com.hearus.hearusspring.data.dto.SocketMsgDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Component
@@ -21,15 +23,57 @@ public class NettySocketio {
         this.namespace = server.addNamespace("/socketio");
         this.namespace.addConnectListener(onConnected());
         this.namespace.addDisconnectListener(onDisconnected());
-        this.namespace.addEventListener("stt", SocketMsgDTO.class, onReceived());
+        this.namespace.addEventListener("transcription", SocketMsgDTO.class, transcribe());
     }
 
-    private DataListener<SocketMsgDTO> onReceived() {
+    private DataListener<SocketMsgDTO> transcribe() {
         return (client, data, ackSender) -> {
-            log.info("[NettySocketio]-[onReceived]-[{}] Received message '{}'", client.getSessionId().toString(), data);
+            log.info("[NettySocketio]-[onReceived]-[{}] Received Audio Data", client.getSessionId().toString());
+
+            byte[] audioData = data.getAudioData();
+
+            new Thread(() -> {
+                String textResult = sendAudioDataToFastAPI(audioData);
+
+                // 3. FastAPI로부터 응답된 텍스트를 새로운 SocketMsgDTO 객체에 담아 전송
+                SocketMsgDTO responseDTO = new SocketMsgDTO();
+                responseDTO.setMessage(textResult);
+                namespace.getBroadcastOperations().sendEvent("stt", responseDTO);
+            }).start();
             namespace.getBroadcastOperations().sendEvent("stt", data);
         };
     }
+
+    private String sendAudioDataToFastAPI(byte[] audioData) {
+        // 1. RestTemplate 인스턴스 생성
+        RestTemplate restTemplate = new RestTemplate();
+
+        // 2. 요청 헤더 설정 (옵션)
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+
+        // 3. 요청 본문 설정
+        byte[] requestBody = audioData;
+
+        // 4. POST 요청 전송 및 응답 객체 받기
+        ResponseEntity<String> responseEntity = restTemplate.exchange(
+                "<FASTAPI_ENDPOINT_URL>",
+                HttpMethod.POST,
+                new HttpEntity<>(requestBody, headers),
+                String.class
+        );
+
+        // 5. 응답 결과 파싱 및 텍스트 추출
+        try {
+            JSONObjectNode responseJSON = new JSONObject(responseEntity.getBody());
+            String text = responseJSON.getString("text");
+            return text;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
 
     private ConnectListener onConnected() {
         return client -> {
--- src/main/java/com/hearus/hearusspring/data/dto/SocketMsgDTO.java
+++ src/main/java/com/hearus/hearusspring/data/dto/SocketMsgDTO.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class SocketMsgDTO {
     String message;
+    byte[] audioData;
 
     @Override
     public String toString(){
