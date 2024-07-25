9e7f13cf8f1984a45184c5fedade4468086599ad
unknown <judemin2087@naver.com>
2024-07-25 15:04:41 +0900

    [ADD] : ADD HomeController, Home HTML Page

---
+++ src/main/java/com/hearus/hearusspring/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.hearus.hearusspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
