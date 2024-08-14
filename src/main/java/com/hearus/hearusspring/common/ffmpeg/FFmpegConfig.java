fd1ccc9ae03569725a818527ecc3d8ac7344a106
unknown <judemin2087@naver.com>
2024-08-14 17:40:37 +0900

    [UPDATE] UPDATED FFmpegConfig

--- src/main/java/com/hearus/hearusspring/common/ffmpeg/FFmpegConfig.java
+++ src/main/java/com/hearus/hearusspring/common/ffmpeg/FFmpegConfig.java
@@ -1,8 +1,10 @@
 package com.hearus.hearusspring.common.ffmpeg;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class FFmpegConfig {
 
@@ -10,6 +12,7 @@ public class FFmpegConfig {
     private String activeProfile;
 
     public String getFFmpegPath() {
+        log.info("[FFmpegConfig]-[getFFmpegPath] {}", activeProfile);
         if ("production".equals(activeProfile)) {
             // Docker 환경에서는 시스템 경로의 FFmpeg 사용
             return "ffmpeg";
