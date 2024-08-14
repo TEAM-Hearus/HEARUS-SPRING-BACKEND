4808e5a966419e0a7541d05c982103f7a880dca2
unknown <judemin2087@naver.com>
2024-08-14 20:03:05 +0900

    [UPDATE] : UPDATED AudioConverter Tempfile Permission setting

--- src/main/java/com/hearus/hearusspring/common/ffmpeg/AudioConverter.java
+++ src/main/java/com/hearus/hearusspring/common/ffmpeg/AudioConverter.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.attribute.PosixFilePermissions;
 
 @Slf4j
 @Component
@@ -41,6 +43,10 @@ public class AudioConverter {
         // Create a temporary file to store the converted audio data
         File tempOutputFile = File.createTempFile("temp_audio_output", ".raw");
         tempOutputFile.deleteOnExit();
+
+        // Set read and write permissions for owner, group, and others
+        Files.setPosixFilePermissions(tempOutputFile.toPath(),
+                PosixFilePermissions.fromString("rw-rw-rw-"));
 
         // Use FFmpeg to convert the audio data
         FFmpeg ffmpeg = new FFmpeg("ffmpeg");
