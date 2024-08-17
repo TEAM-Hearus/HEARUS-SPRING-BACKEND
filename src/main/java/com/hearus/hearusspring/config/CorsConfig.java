e84338aca479154f733f4aa1de191d6f92c94456
unknown <judemin2087@naver.com>
2024-08-17 14:21:39 +0900

    [UPDATE] : UPDATED CORS Config not wallowing Credential with Wild Card AllowedOrigin

--- src/main/java/com/hearus/hearusspring/config/CorsConfig.java
+++ src/main/java/com/hearus/hearusspring/config/CorsConfig.java
@@ -14,8 +14,19 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://127.0.0.1:5173", "https://www.hearus.site", "https://hearus.site"));
+        // 개발 단게에서는 setAllowCredentials(false) 로 설정
+        // Setting this property has an impact on how origins, originPatterns, allowedMethods and allowedHeaders are processed
+        // Be aware that this option establishes a high level of trust with the configured domains
+        // and also increases the surface attack of the web application
+        config.setAllowCredentials(false);
+
+        // setAllowCredentials(true)일 경우 "*"로 설정 불가
+        config.addAllowedOrigin("*");
+
+        // 동일 출처를 판단하는 기준 중 포트 번호를 명시한 출처는
+        // 브라우저의 구현 로직에 따라 동일 출처로 판단하지 않을 수 있기 때문에
+        // 포트번호를 생략한 URL을 CORS 설정의 허용 출처에 추가
+        // config.setAllowedOrigins(Arrays.asList("http://localhost", "http://localhost:5173", "https://www.hearus.site", "https://hearus.site"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setExposedHeaders(Arrays.asList("Authorization", "Content-Type","Cookie","Set-Cookie"));
