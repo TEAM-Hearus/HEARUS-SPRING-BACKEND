75a589249cc593858d63ecd4238172550b3942ee
kangchanwook <kan0202@naver.com>
2024-09-30 18:13:56 +0900

    Update : CORS error fix
    CookieUtils 의 Cors 설정 변경

--- src/main/java/com/hearus/hearusspring/data/oauth/repository/CookieUtils.java
+++ src/main/java/com/hearus/hearusspring/data/oauth/repository/CookieUtils.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 import org.springframework.util.SerializationUtils;
-
 import java.util.Base64;
 import java.util.Optional;
 
@@ -32,7 +31,9 @@ public class CookieUtils {
         cookie.setMaxAge(maxAge);
         cookie.setDomain("localhost");
         response.addCookie(cookie);
-        response.setHeader("Set-Cookie", name+"="+value+"; "+"Path=/; Secure; HttpOnly; SameSite=None");
+        response.addHeader("Access-Control-Allow-Origin", "https://hearus.site");
+        response.addHeader("Access-Control-Allow-Credentials", "true");
+        response.addHeader("Set-Cookie", name+"="+value+"; "+"Path=/; Secure; HttpOnly; SameSite=None");
     }
 
     public void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
