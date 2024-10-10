7dfbdf18ddf683d22f50b95101112db011df55f9
kangchanwook <kan0202@naver.com>
2024-10-11 00:10:42 +0900

    [UPDATE] : UPDATE validation of UserLoginDTO and UserSignupDTO
    Add @Min annotaion

--- src/main/java/com/hearus/hearusspring/data/dto/user/UserLoginDTO.java
+++ src/main/java/com/hearus/hearusspring/data/dto/user/UserLoginDTO.java
@@ -2,6 +2,7 @@ package com.hearus.hearusspring.data.dto.user;
 
 import com.hearus.hearusspring.common.enumType.RoleType;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
@@ -13,6 +14,7 @@ public class UserLoginDTO {
     @NotBlank(message = "userEmail not found in request")
     String userEmail;
     @NotBlank(message = "userPassword not found in request")
+    @Min(value = 0, message = "userPassword is too short")
     String userPassword;
     boolean userIsOAuth = false;
 
--- src/main/java/com/hearus/hearusspring/data/dto/user/UserSignupDTO.java
+++ src/main/java/com/hearus/hearusspring/data/dto/user/UserSignupDTO.java
@@ -3,6 +3,7 @@ package com.hearus.hearusspring.data.dto.user;
 import com.hearus.hearusspring.common.enumType.RoleType;
 import jakarta.persistence.Entity;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
@@ -14,9 +15,13 @@ public class UserSignupDTO {
     @Email(message = "Email format is incorrect")
     @NotBlank(message = "userEmail not found in request")
     String userEmail;
+
     @NotBlank(message = "userPassword not found in request")
+    @Min(value = 0, message = "userPassword is too short")
     String userPassword;
+
     boolean userIsOAuth = false;
+
     @NotBlank(message = "userName not found in request")
     String userName;
 
